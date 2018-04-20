796b12262a3d1f75f5adb4a8b8a99c145683d4e0
natamate <naciaa.nm@gmail.com>
2018-04-20 18:22:14 +0200

    Add list of actors

--- SmartGunWeb/SmartGunWeb/Controllers/PolicemanController.cs
+++ SmartGunWeb/SmartGunWeb/Controllers/PolicemanController.cs
@@ -19,6 +19,7 @@ namespace SmartGunWeb.Controllers
             return loggedPoliceman;
         }
 
+        //GET api/policeman/id
         public Policeman GetPolicemanById(int id)
         {
             var policeman = allPoliceman.FirstOrDefault((p) => p.Id == id);
@@ -36,9 +37,8 @@ namespace SmartGunWeb.Controllers
                 StringComparison.OrdinalIgnoreCase));
 
         // POST api/policeman/{id}/{password}
-        public HttpResponseMessage Post(int id, [FromBody]string password)
+        public HttpResponseMessage Post(int id, string password)
         {
-            int userId;
             HttpResponseMessage response =
                 Request.CreateResponse(HttpStatusCode.Forbidden, "Error during registration"); 
             response.Content = new StringContent("The user with " + id + "can't be created", Encoding.Unicode);
@@ -66,7 +66,7 @@ namespace SmartGunWeb.Controllers
         }
 
         // PUT api/policeman/id/{password}
-        public HttpResponseMessage Put(int id, [FromBody]string password)
+        public HttpResponseMessage Put(int id, string password)
         {
             HttpResponseMessage response =
                 Request.CreateResponse(HttpStatusCode.Forbidden, "Error during sign in");
@@ -75,7 +75,7 @@ namespace SmartGunWeb.Controllers
             var policeman = allPoliceman.FirstOrDefault((p) => p.Id == id);
             if (policeman == null)
             {
-                response.Content = new StringContent("The user with " + id + "is not exist", Encoding.Unicode);
+                response.Content = new StringContent("The user with " + id + " is not exist", Encoding.Unicode);
                 return response;
             }
 
@@ -106,8 +106,40 @@ namespace SmartGunWeb.Controllers
             return response;
         }
 
+        // PUT api/policeman/id/{status}
+        public HttpResponseMessage Put(int id, Status status)
+        {
+            HttpResponseMessage response =
+                Request.CreateResponse(HttpStatusCode.Forbidden, "Error during changing status");
+            response.Content = new StringContent("Change status is failed", Encoding.Unicode);
+
+            var policeman = allPoliceman.FirstOrDefault((p) => p.Id == id);
+            if (policeman == null)
+            {
+                response.Content = new StringContent("The user with " + id + " is not exist", Encoding.Unicode);
+                return response;
+            }
+
+            allPoliceman.Remove(policeman);
+            Boolean isLogged = loggedPoliceman.Contains(policeman);
+            if (isLogged)
+            {
+                loggedPoliceman.Remove(policeman);
+            }
+
+            policeman.Status = status;
+            allPoliceman.Add(policeman);
+            if (isLogged)
+                loggedPoliceman.Add(policeman);
+
+            response = Request.CreateResponse(HttpStatusCode.OK, "The user with " + id + " change status");
+            response.Content = new StringContent("Change status succesfully", Encoding.Unicode);
+
+            return response;
+        }
+
         //DELETE api/policeman/id
-        public HttpResponseMessage SignOut(int id)
+        public HttpResponseMessage Delete(int id)
         {
             HttpResponseMessage response =
                 Request.CreateResponse(HttpStatusCode.Forbidden, "Error during sign out");
