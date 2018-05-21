e6129f1969ae2f0d152820f9b5985ef55f76a77c
aleklisi <sebaczechu123@gmail.com>
2018-05-21 13:02:58 +0200

    adding CORE service

---
+++ SmartGun/CORE/Logics/DistanceStrategies/IDistanceFindStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Database.Model;
+
+namespace CORE.Logics.DistanceStrategies
+{
+    interface IDistanceFindStrategy
+    {
+        List<Policeman> GetClosestPolicemen(List<Policeman> policeforce, double x, double y);
+    }
+}
---
+++ SmartGun/CORETest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CORE;
+using PointOfContact;
+
+namespace CORETest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var core = new CoreService();
+            core.HandleWarning(new WarningMessage(2, 2, "Bane"));
+            core.HandleShot(new ShotMessage(2, 2, "Aane"));
+            Console.ReadKey();
+        }
+    }
+}
